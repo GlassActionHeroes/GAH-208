25213ec4f3e17010ccd6ca49bf2f5bf71c1e3115
Chae O'Keefe <chaemobile@gmail.com>
2014-04-24 18:24:14 -0400

    Change route endpoint

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -2,11 +2,7 @@ class PusherController < ApplicationController
   protect_from_forgery :except => :auth # stop rails CSRF protection for this action
 
   def auth
-    if params[:socket_id] == ENV['PUSHER_KEY']
-      response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
-      render :json => response
-    else
-      render :text => "Forbidden", :status => '403'
-    end
+    response = Pusher[params[:channel_name]].authenticate(params[:socket_id])
+    render :json => response
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
- get '/hello_world' => "hello_world#hello_world"
- get '/auth' => "pusher#auth"
+  get '/hello_world' => 'hello_world#hello_world'
+  post 'pusher/auth'
 end
